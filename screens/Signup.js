7e2bca38afc5be40a885c9bafd217d26f875aa76
Prathima0708 <kotianpriya64@gmail.com>
2023-11-27 12:29:26 +0530

    integrated signup

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity, ImageBackground } from 'react-native'
 import React, { useState, useReducer, useEffect, useCallback } from 'react'
 import { COLORS, images } from '../constants'
-import * as Animatable from "react-native-animatable"
+import * as Animatable from 'react-native-animatable'
 import Input from '../components/Input'
 import Button from '../components/Button'
 import { validateInput } from '../utils/actions/formActions'
@@ -9,8 +9,9 @@ import { reducer } from '../utils/reducers/formReducers'
 import { commonStyles } from '../styles/CommonStyles'
 import { StatusBar } from 'expo-status-bar'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons'
 import { LinearGradient } from 'expo-linear-gradient'
+import { login_URL, reg_URL } from '../constants/utils/URL'
 
 const isTestMode = true
 
@@ -28,8 +29,8 @@ const initialState = {
         email: false,
         password: false,
         confirmPassword: false,
-        companyName:false,
-        address1:false
+        companyName: false,
+        address1: false,
     },
     formIsValid: false,
 }
@@ -38,40 +39,91 @@ const Signup = ({ navigation }) => {
     const [error, setError] = useState()
     const [isLoading, setIsLoading] = useState(false)
     const [formState, dispatchFormState] = useReducer(reducer, initialState)
+    const [formData, setFormData] = useState({
+        fullName: '',
+        email: '',
+        password: '',
+    })
 
-    const inputChangedHandler = useCallback(
-        (inputId, inputValue) => {
-            const result = validateInput(inputId, inputValue)
-            dispatchFormState({ inputId, validationResult: result, inputValue })
-        },
-        [dispatchFormState]
-    )
+    // const inputChangedHandler = useCallback(
+    //     (inputId, inputValue) => {
+    //         const result = validateInput(inputId, inputValue)
+    //         dispatchFormState({ inputId, validationResult: result, inputValue })
+    //     },
+    //     [dispatchFormState]
+    // )
+
+    const inputChangedHandler = (id, value) => {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [id]: value,
+        }))
+    }
 
     useEffect(() => {
         if (error) {
             Alert.alert('An error occured', error)
         }
     }, [error])
+    const onSubmit = async () => {
+        try {
+            setIsLoading(true)
+
+            const response = await fetch(`${reg_URL}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    fullName: formData.fullName,
+                    email: formData.email,
+                    password: formData.password,
+                }),
+            })
+
+            if (response.ok) {
+                const responseData = await response.json()
+                console.log('API response:', responseData)
+                // Handle successful signup, e.g., navigate to the login screen
+                navigation.navigate('Login')
+            } else {
+                throw new Error('Signup failed')
+            }
+        } catch (error) {
+            console.error('Error during signup:', error)
+            // Handle error, e.g., display an error message to the user
+        } finally {
+            setIsLoading(false)
+        }
+    }
 
     return (
         <LinearGradient
             colors={[COLORS.primary, COLORS.primary]}
-            style={{ flex: 1, backgroundColor: COLORS.blue }}>
+            style={{ flex: 1, backgroundColor: COLORS.blue }}
+        >
             <StatusBar hidden={true} />
             <View style={commonStyles.header}>
                 <TouchableOpacity
                     onPress={() => navigation.goBack()}
-                    style={commonStyles.backIcon}>
-                    <MaterialIcons name="keyboard-arrow-left" size={24} color={COLORS.black} />
+                    style={commonStyles.backIcon}
+                >
+                    <MaterialIcons
+                        name="keyboard-arrow-left"
+                        size={24}
+                        color={COLORS.black}
+                    />
                 </TouchableOpacity>
                 <Text style={commonStyles.headerTitle}>Sign up</Text>
-                <Text
-                    style={commonStyles.subHeaderTitle}>Please sign up to get started</Text>
+                <Text style={commonStyles.subHeaderTitle}>
+                    Please sign up to get started
+                </Text>
             </View>
             <Animatable.View
                 animation="fadeInUpBig"
-                style={commonStyles.footer}>
-                < KeyboardAwareScrollView>
+                style={commonStyles.footer}
+            >
+                <KeyboardAwareScrollView>
                     <Text style={commonStyles.inputHeader}>Name</Text>
                     <Input
                         id="fullName"
@@ -100,7 +152,9 @@ const Signup = ({ navigation }) => {
                         secureTextEntry={true}
                     />
 
-                    <Text style={commonStyles.inputHeader}>Re-Type Password</Text>
+                    <Text style={commonStyles.inputHeader}>
+                        Re-Type Password
+                    </Text>
                     <Input
                         onInputChanged={inputChangedHandler}
                         errorText={formState.inputValidities['passwordConfirm']}
@@ -110,7 +164,7 @@ const Signup = ({ navigation }) => {
                         placeholderTextColor={COLORS.black}
                         secureTextEntry={true}
                     />
-                   <Text style={commonStyles.inputHeader}>Company Name</Text>
+                    {/* <Text style={commonStyles.inputHeader}>Company Name</Text>
                     <Input
                         id="companyName"
                         onInputChanged={inputChangedHandler}
@@ -166,12 +220,13 @@ const Signup = ({ navigation }) => {
                         errorText={formState.inputValidities['pincode']}
                         placeholder="576104"
                         placeholderTextColor={COLORS.black}
-                    />
+                    /> */}
                     <Button
                         title="SIGN UP"
                         isLoading={isLoading}
                         filled
-                        onPress={() => navigation.navigate('Login')}
+                        onPress={onSubmit}
+                        //  onPress={() => navigation.navigate('Login')}
                         style={commonStyles.btn1}
                     />
                 </KeyboardAwareScrollView>
